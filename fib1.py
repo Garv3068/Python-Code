d454466338e1d4d295b9601f19e86337247b2a85
Garv3068 <garv3068.beai25@chitkara.edu.in>
2025-12-16 06:53:54 +0000

    Recursive

---
+++ fib1.py
@@ -0,0 +1,10 @@
+from functools import lru_cache
+def fib(n):
+    if n==0:
+        return 1
+    if n==1:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+for i in range(40):
+    print(fib(i))
